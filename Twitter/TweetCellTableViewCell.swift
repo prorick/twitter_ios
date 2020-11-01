8d6bd67c51f108c606fb37923e31ef163d8b6c6e
prorick <56745348+prorick@users.noreply.github.com>
2020-11-01 01:17:36 -0800

    Part 1 of Twitter Project. Added Login/Logout functionality. User can view tweets with the user profile picture, username, and tweet text

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Prothit Halder on 11/1/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
